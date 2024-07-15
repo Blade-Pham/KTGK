91e71aecb262d70a5a521b732548ac6667fef1a7
PhamAnh <phamanh062003@gmail.com>
2024-07-15 09:29:39 +0700

    Add project files.

---
+++ FormGK2.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace KTGK
+{
+    partial class FormGK2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rb_all = new System.Windows.Forms.RadioButton();
+            this.rb_CTDT = new System.Windows.Forms.RadioButton();
+            this.cb_CTDT = new System.Windows.Forms.ComboBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.dataGridView1);
+            this.groupBox1.Controls.Add(this.cb_CTDT);
+            this.groupBox1.Controls.Add(this.rb_CTDT);
+            this.groupBox1.Controls.Add(this.rb_all);
+            this.groupBox1.Location = new System.Drawing.Point(22, 29);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(766, 534);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Thông tin sinh viên";
+            // 
+            // rb_all
+            // 
+            this.rb_all.AutoSize = true;
+            this.rb_all.Location = new System.Drawing.Point(37, 39);
+            this.rb_all.Name = "rb_all";
+            this.rb_all.Size = new System.Drawing.Size(78, 24);
+            this.rb_all.TabIndex = 0;
+            this.rb_all.TabStop = true;
+            this.rb_all.Text = "Tất cả";
+            this.rb_all.UseVisualStyleBackColor = true;
+            // 
+            // rb_CTDT
+            // 
+            this.rb_CTDT.AutoSize = true;
+            this.rb_CTDT.Location = new System.Drawing.Point(156, 39);
+            this.rb_CTDT.Name = "rb_CTDT";
+            this.rb_CTDT.Size = new System.Drawing.Size(115, 24);
+            this.rb_CTDT.TabIndex = 0;
+            this.rb_CTDT.TabStop = true;
+            this.rb_CTDT.Text = "Theo CTDT";
+            this.rb_CTDT.UseVisualStyleBackColor = true;
+            this.rb_CTDT.CheckedChanged += new System.EventHandler(this.rb_CTDT_CheckedChanged);
+            // 
+            // cb_CTDT
+            // 
+            this.cb_CTDT.FormattingEnabled = true;
+            this.cb_CTDT.Location = new System.Drawing.Point(300, 34);
+            this.cb_CTDT.Name = "cb_CTDT";
+            this.cb_CTDT.Size = new System.Drawing.Size(130, 28);
+            this.cb_CTDT.TabIndex = 1;
+            this.cb_CTDT.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5});
+            this.dataGridView1.Location = new System.Drawing.Point(0, 114);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(766, 420);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // Column1
+            // 
+            this.Column1.DataPropertyName = "sMaSV";
+            this.Column1.HeaderText = "Mã Sinh Viên";
+            this.Column1.MinimumWidth = 8;
+            this.Column1.Name = "Column1";
+            this.Column1.Width = 150;
+            // 
+            // Column2
+            // 
+            this.Column2.DataPropertyName = "sHoTen";
+            this.Column2.HeaderText = "Họ và tên";
+            this.Column2.MinimumWidth = 8;
+            this.Column2.Name = "Column2";
+            this.Column2.Width = 150;
+            // 
+            // Column3
+            // 
+            this.Column3.DataPropertyName = "dNgaySinh";
+            this.Column3.HeaderText = "Ngày sinh";
+            this.Column3.MinimumWidth = 8;
+            this.Column3.Name = "Column3";
+            this.Column3.Width = 150;
+            // 
+            // Column4
+            // 
+            this.Column4.DataPropertyName = "sTenHocPhan";
+            this.Column4.HeaderText = "Tên Môn Học";
+            this.Column4.MinimumWidth = 8;
+            this.Column4.Name = "Column4";
+            this.Column4.Width = 150;
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "Điểm Môn Học";
+            this.Column5.MinimumWidth = 8;
+            this.Column5.Name = "Column5";
+            this.Column5.Width = 150;
+            // 
+            // FormGK2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 575);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "FormGK2";
+            this.Text = "FormGK2";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ComboBox cb_CTDT;
+        private System.Windows.Forms.RadioButton rb_CTDT;
+        private System.Windows.Forms.RadioButton rb_all;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+    }
+}
+
---
+++ FormGK2.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KTGK
+{
+    public partial class FormGK2 : Form
+    {
+  
+        public FormGK2()
+        {
+            InitializeComponent();
+            rb_all.Checked = true;
+            cb_CTDT.Enabled = false;
+            LoadAllStudents();
+
+
+        }
+
+        private void rb_CTDT_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rb_CTDT.Checked)
+            {
+                cb_CTDT.Enabled = true;
+                LoadComboBox();
+            }
+            else
+            {
+                cb_CTDT.Enabled = false;
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            LoadStudentsByCTDT(cb_CTDT.SelectedValue.ToString());
+        }
+        private void LoadComboBox()
+        {
+            string connectionString = @"Data Source=DESKTOP-3FLNI2M;Initial Catalog=KTGK;Integrated Security=True";
+            string query = "SELECT sMaCTDT FROM tblCTDT";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    SqlCommand command = new SqlCommand(query, connection);
+                    SqlDataReader reader = command.ExecuteReader();
+
+                    DataTable dataTable = new DataTable();
+                    dataTable.Load(reader);
+
+                    cb_CTDT.DataSource = dataTable;
+                    cb_CTDT.DisplayMember = "sMaCTDT";
+                    cb_CTDT.ValueMember = "sMaCTDT";
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: " + ex.Message);
+                }
+            }
+        }
+        private void LoadAllStudents()
+        {
+            string connectionString = @"Data Source=DESKTOP-3FLNI2M;Initial Catalog=KTGK;Integrated Security=True";
+            string query = @"
+               SELECT 
+    tblSINHVIEN.sMaSV, 
+    sHoTen, 
+    dNgaySinh, 
+    tblMONHOC.sTenHocPhan AS MonHoc, 
+    (0.3 * fDiemQT + 0.7 * fDiemCK) AS DiemMonHoc
+FROM tblSINHVIEN
+JOIN tblDIEM ON tblSINHVIEN.sMaSV = tblDIEM.sMaSV
+JOIN tblMONHOC ON tblDIEM.sMaHocPhan = tblMONHOC.sMaHocPhan";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    SqlCommand command = new SqlCommand(query, connection);
+                    SqlDataAdapter adapter = new SqlDataAdapter(command);
+                    DataTable dataTable = new DataTable();
+                    adapter.Fill(dataTable);
+                    dataGridView1.DataSource = dataTable;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: " + ex.Message);
+                }
+            }
+        }
+
+        private void LoadStudentsByCTDT(string maHocPhan)
+        {
+            string connectionString = @"Data Source=DESKTOP-3FLNI2M;Initial Catalog=KTGK;Integrated Security=True";
+            string query = @"
+                SELECT 
+                    tblSINHVIEN.sMaSV, 
+                    tblSINHVIEN.sHoTen, 
+                    tblSINHVIEN.dNgaySinh, 
+                    tblMONHOC.sTenHocPhan AS MonHoc, 
+                    (0.3 * tblDIEM.fDiemQT + 0.7 * tblDIEM.fDiemCK) AS DiemMonHoc
+                FROM tblSINHVIEN
+                JOIN tblDIEM ON tblSINHVIEN.sMaSV = tblDIEM.sMaSV
+                JOIN tblMONHOC ON tblDIEM.sMaHocPhan = tblMONHOC.sMaHocPhan
+                JOIN tblCTDT ON tblMONHOC.sMaCTDT = tblCTDT.sMaCTDT
+                WHERE tblCTDT.sMaCTDT = @MaHocPhan";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    SqlCommand command = new SqlCommand(query, connection);
+                    command.Parameters.AddWithValue("@MaHocPhan", maHocPhan);
+                    SqlDataAdapter adapter = new SqlDataAdapter(command);
+                    DataTable dataTable = new DataTable();
+                    adapter.Fill(dataTable);
+
+                    dataGridView1.DataSource = dataTable;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: " + ex.Message);
+                }
+            }
+        }
+    }
+}
